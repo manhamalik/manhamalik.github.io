d7a395d81a85708517f45e8c08d75d16eb89e890
Manha Malik <manhamalik77@gmail.com>
2024-10-24 19:18:16 -0400

    fixes

---
+++ config.js
@@ -0,0 +1,4 @@
+const RECAPTCHA_SITE_KEY='6Lc6nWoqAAAAAKIicXUiSxBtD5lw35cmOjdaQa_l';
+const EMAILJS_PUBLIC_KEY='bKHfJVpjOmwVgy-Qv';
+const EMAILJS_SERVICE_ID='service_2fubcba';
+const EMAILJS_TEMPLATE_ID='template_2u0z9ea';
